d53c947c4ca7487c4941ca389c83e17cb698dc0a
madhukar01 <mholla8@gmail.com>
2022-05-29 02:05:10 +0530

    feat(login): web3 login

---
+++ js/auth.js
@@ -0,0 +1,42 @@
+/* Metamask authentication code */
+async function metamaskLogin() {
+    let user = Moralis.User.current();
+    console.log(user);
+    if (!user) {
+        user = await Moralis.authenticate({
+            signingMessage: "Log in to Eggplant",
+            chainId: 80001
+        })
+            .then(function (user) {
+                console.log("logged in user:", user);
+                console.log(user.get("ethAddress"));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
+
+/* Walletconnect authentication */
+async function walletconnectLogin() {
+    let user = Moralis.User.current();
+    if (!user) {
+        user = await Moralis.authenticate({
+            provider: "walletconnect",
+            signingMessage: "Log in to Eggplant",
+            chainId: 80001
+        })
+            .then(function (user) {
+                console.log("logged in user:", user);
+                console.log(user.get("ethAddress"));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
+
+async function logOut() {
+    await Moralis.User.logOut();
+    console.log("logged out");
+}
---
+++ js/index.js
@@ -0,0 +1,3 @@
+const serverUrl = "https://pjsctz5bkqjc.usemoralis.com:2053/server";
+const appId = "TBgozxOyrhwNWSqwPv6bKw0ZDb2PsH52d7GxEHRm";
+Moralis.start({ serverUrl, appId });
